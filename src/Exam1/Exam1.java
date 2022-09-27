c8af87917fc42e2e54d57529e4de3fd31b508039
slhess20 <sierralindahess@gmail.com>
2022-09-27 15:37:46 -0400

    Exam 1

---
+++ src/Exam1/Exam1.java
@@ -0,0 +1,33 @@
+package Exam1;
+
+import java.util.Scanner;
+
+public class Exam1 {
+    public static void main(String [] args) {
+        Scanner s = new Scanner(System.in);
+        boolean valid_n = false;
+        int n = 0;
+        while (!valid_n) {
+            System.out.print("Please enter a positive integer:");
+            if (s.hasNextDouble()) {
+                n = s.nextInt();
+                if (n < 0) {
+                    System.out.printf("Error: Integer cannot be negative. You entered %.2d\n", n);
+                } else {
+                    valid_n = true;
+                }
+            } else {
+                System.out.printf("Error: You must enter a positive integer. You entered \"%s\"\n", s.next());
+            }
+        }
+        int count = 0;
+
+        while (n > 0) {
+            if (n % 10 <= 7 && n % 10 >= 2)
+                count++;
+            n = n / 10;
+        }
+        System.out.print("Number of digits between 2 and 7:  ");
+        System.out.print(count);
+    }
+}
